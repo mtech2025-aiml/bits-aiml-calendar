f5bf1996f879c61c662f1d8d98a5a83f57a0b27c
SanSan <dabral.pavitra1967@gmail.com>
2026-05-20 19:56:02 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 window.APP_CONFIG = {
   apiBase: "https://script.google.com/macros/s/AKfycbyrJ3yBSwksqvqykdUWW6M9HUTjrmcRo3hDGhg4o2q0EyLke-67NEiomTEQ5rCbYdbx/exec",
   defaultSemester: "2",
-  defaultElective: "",
   defaultType: "All"
 };
