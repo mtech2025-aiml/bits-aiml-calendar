5b238c53bff4f2ecd850348d8cbbbbbcc5238c1a
SanSan <dabral.pavitra1967@gmail.com>
2026-05-20 20:52:34 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,8 @@
 window.APP_CONFIG = {
   apiBase: "https://script.google.com/macros/s/AKfycbyrJ3yBSwksqvqykdUWW6M9HUTjrmcRo3hDGhg4o2q0EyLke-67NEiomTEQ5rCbYdbx/exec",
   defaultSemester: "2",
-  defaultType: "All"
+  defaultType: "All",
+  defaultView: "ec1",
+  classScheduleImageUrl: "https://drive.google.com/uc?export=view&id=10iKBANaPW74PAc8xZ-qQyYvEbKLNbmOp",
+  examGuideImageUrl: "assets/exam-guide.png"
 };
